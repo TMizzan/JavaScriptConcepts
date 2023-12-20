1c6eefbbe766f882d4888102b434b2255a1f984b
TMizzan <terrellrhodes@hotmail.com>
2023-12-20 13:44:50 -0500

    Variable Concept Updates
    Variable Concept Updates

--- JS/variableConcepts.js
+++ JS/variableConcepts.js
@@ -5,19 +5,19 @@ DisplayNodeJSInformation();
 
 // Data Types
 let firstName = "";     // String
-let count = 4;          // Number
-let x = true;           // Boolean 
-let y;                  // Undefined
-let z = null;           // Null
-let range = [];         // Array
+let count     = 4;      // Number
+let x         = true;   // Boolean 
+let y         = null;   // Null
+let z;                  // Undefined
+let range     = [];     // Array
 
 let person = { City : "State",          // Object
                Zip  : "29210" };
 
-let dt = new Date("2024-01-31");        // Date
-let a = 123e5;                          // Exponent 12300000 
-let b = 123e-5;                         // Exponent 0.00123
-let c = BigInt("1234567890123456789");  // Big Int
+let dt = new Date("2024-01-31");         // Date
+let a  = 123e5;                          // Exponent 12300000 
+let b  = 123e-5;                         // Exponent 0.00123
+let c  = BigInt("1234567890123456789");  // Big Int
 
 LogMessage('Undefined : ' + typeof(undefined));  // Typeof Undefined Is Undefined
-LogMessage('Null : '      + typeof(null));       // Typeof Null Is Object
+LogMessage('Null      : ' + typeof(null));       // Typeof Null Is Object
