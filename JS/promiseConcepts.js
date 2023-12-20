e71f5dd9658f7ae91edc499a99b0915818b5a769
TMizzan <terrellrhodes@hotmail.com>
2023-12-20 14:15:49 -0500

    Promise Concepts Update
    Promise Concepts Update

--- JS/promiseConcepts.js
+++ JS/promiseConcepts.js
@@ -2,3 +2,36 @@ import { CheckNodeJSVersion, DisplayNodeJSInformation, GetNodeJSVersion, LogMess
 
 // Display NodeJS Information
 DisplayNodeJSInformation();
+
+
+// Promise
+LogMessage("Promise Example");
+LogMessage();
+
+let vehiclePurchasePromise = new Promise(function (resolve, reject) { 
+    
+    let min = 1;
+    let max = 5;
+    let purchaseDay = Math.floor((Math.random() * (max - min + 1) + min));
+
+    if (purchaseDay >= 5) {
+        resolve(`The Purchase Date (${purchaseDay}) On Or Later Than 5.`);
+    } else {
+        reject(`The Purchase Date (${purchaseDay}) Earlier Than 5.`);
+    }
+    
+});
+
+
+vehiclePurchasePromise.
+    then(
+        function (successMessage) {
+            LogMessage(`Success Message : ${successMessage}`);
+        }, 
+        function (rejectMessage) {
+            LogMessage(`Reject Message  : ${rejectMessage}`);
+        })
+        .catch(function(errorMessage) {
+            LogMessage(`Error Message   : ${errorMessage}`);
+        });
+
