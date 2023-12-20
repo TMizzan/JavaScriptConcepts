deffb0dd8f4841821484546d2d01ba0774d9b209
TMizzan <terrellrhodes@hotmail.com>
2023-12-20 13:25:56 -0500

    Object Concept Updates
    Object Concept Updates

--- JS/objectConcepts.js
+++ JS/objectConcepts.js
@@ -62,6 +62,54 @@ catch (ex) {
 LogMessage();
 
 
+// FromEntries
+const speedBoatEntries = new Map ([['name', 'WaveRunner'], ['type', 'speed']]);
+const speedBoat = Object.fromEntries(speedBoatEntries);
+LogMessage('FromEntries Example');
+LogMessage(`FromEntries : SpeedBoat Name: ${speedBoat.name}, SpeedBoat Type: ${speedBoat.type}`);
+LogMessage();
+
+
+// GetOwnPropertyDescriptor 
+LogMessage('GetOwnPropertyDescriptor Example');
+const passengersDescription = Object.getOwnPropertyDescriptor(moped, 'passengers');
+LogMessage(`GetOwnPropertyDescriptor Passengers Configurable: ${passengersDescription.configurable}`);
+LogMessage();
+
+
+// GetOwnPropertyDescriptors
+LogMessage('GetOwnPropertyDescriptors Example');
+const seatsDescription = Object.getOwnPropertyDescriptor(moped, 'seats');
+LogMessage(`GetOwnPropertyDescriptors Seats Writable       : ${passengersDescription.writable}`);
+LogMessage(`GetOwnPropertyDescriptors Seats Value Writable : ${passengersDescription.value}`);
+LogMessage();
+
+
+
+// GetOwnPropertyNames
+LogMessage('GetOwnPropertyNames Example');
+LogMessage(`GetOwnPropertyNames Moped : ${Object.getOwnPropertyNames(moped)}`);
+LogMessage();
+
+
+// GetOwnPropertySymbols
+LogMessage('GetOwnPropertySymbols Example');
+const symbolClass   = Symbol('class');
+const symbolWeight  = Symbol.for('weight');
+moped[symbolClass]  = "A";
+moped[symbolWeight] = "250";
+const mopedSymbols  = Object.getOwnPropertySymbols(moped);
+LogMessage(`GetOwnPropertySymbols : ${mopedSymbols.length}`);
+LogMessage(moped);
+LogMessage();
+
+
+// GetPrototypeOf
+LogMessage('GetPrototypeOf Example');
+LogMessage(`GetPrototypeOf harleyDavidson>motorcycle : ${Object.getPrototypeOf(harleyDavidson) === motorcycle}`);
+LogMessage();
+
+
 // HasOwnProperty
 LogMessage('HasOwnProperty Example');
 const plane = { model : "Cessna", year  : "2024" };
@@ -103,22 +151,100 @@ LogMessage('IsPrototype Example');
 class Vehicle {}
 class Car extends Vehicle {}
 class SportsCar extends Car {}
-let vehicle = new Vehicle();
-let car = new Car();
+let vehicle   = new Vehicle();
+let car       = new Car();
 let sportsCar = new SportsCar();
 // vehicle   : Vehicle > Object
 // car       : Car > Vehicle > Object
 // sportsCar : SportsCar > Car > Vehicle > Object 
-LogMessage(`Sports Car - Transportation : ${SportsCar.prototype.isPrototypeOf(vehicle)}`);
-LogMessage(`Car - Sports Car            : ${Car.prototype.isPrototypeOf(sportsCar)}`);
-LogMessage(`Vehicle - Car               : ${Vehicle.prototype.isPrototypeOf(car)}`);
+LogMessage(`Sports Car - Vehicle : ${SportsCar.prototype.isPrototypeOf(vehicle)}`);
+LogMessage(`Car - Sports Car     : ${Car.prototype.isPrototypeOf(sportsCar)}`);
+LogMessage(`Vehicle - Car        : ${Vehicle.prototype.isPrototypeOf(car)}`);
 LogMessage();
 
 
-// Sealed
+// IsSealed
 LogMessage('Sealed Example');
-const sealedClass = {}
+const sealedClass = { name : 'sealed' }
 LogMessage(`Is Sealed                : ${Object.isSealed(sealedClass)}`);
 Object.seal(sealedClass);
 LogMessage(`Is Sealed - After Sealed : ${Object.isSealed(sealedClass)}`);
 LogMessage();
+
+
+// Keys
+LogMessage('Keys Example');
+LogMessage(`Keys motorcycle : ${Object.keys(motorcycle)}`);
+LogMessage();
+
+
+// PreventExtensions
+LogMessage('PreventExtensions Example');
+const racingBike = Object.create(motorcycle);
+Object.preventExtensions(racingBike);
+try {
+    Object.defineProperty(racingBike, 'topSpeed', { value: 220 });
+} catch (ex) {
+    LogMessage(`PreventExtensions Exception : ${ex}`);     
+}
+LogMessage();
+
+
+// PropertyIsEnumerable
+LogMessage('PropertyIsEnumerable Example');
+const motorcycleArray = ['Honda GSX-R'];
+LogMessage(`PropertyIsEnumerable Motorcycle Owners : ${motorcycle.propertyIsEnumerable('wheels')}`);
+LogMessage(`PropertyIsEnumerable Motorcycle Array  : ${motorcycleArray.propertyIsEnumerable(0)}`);
+LogMessage(`PropertyIsEnumerable Motorcycle Array  : ${motorcycleArray.propertyIsEnumerable('length')}`);
+LogMessage();
+
+
+// Seal
+LogMessage('Sealed Example');
+try {
+    delete sealedClass.name;
+} catch (ex) {
+    LogMessage(`Sealed Class : ${ex}`);
+}
+LogMessage();
+
+
+// Object.setPrototypeOf
+// Avoid Use Of SetPrototypeOf. Issues With Performance And Cascading Impact Of Prototype Object.
+// Recommendation Is To Create A New Object With The Correct Prtototype.
+
+
+// ToLocaleString
+LogMessage('ToLocaleString Example');
+const purchaseDate = new Date(Date.UTC(2024, 12, 20, 4, 0, 0));
+const purchasePrice = 35000.55;
+LogMessage(`Purchase Date  : ${purchaseDate.toLocaleString('en-US')}`);
+LogMessage(`Purchase Price : ${purchasePrice.toLocaleString('en-US')}`);
+LogMessage();
+
+
+// Prototype.ToString
+LogMessage('Prototype.ToString Example');
+function Jet(name, year, cost) {
+    this.name = name;
+    this.year = year;
+    this.cost = cost;
+}
+const jet = new Jet('Concorde', '2024', '5,000,000');
+Jet.prototype.toString = function jetToString() { return `${this.year} ${this.name}`; };
+LogMessage(`Prototype.ToString Jet : ${jet.toString()}`);
+LogMessage();
+
+
+// Prototype.ToString
+LogMessage('Prototype.ToString Example');
+Jet.prototype.valueOf = function ()  { return this.cost; };
+LogMessage(`Prototype.ToString ValueOf : ${jet.valueOf()}`);
+LogMessage();
+
+
+// Values
+LogMessage('Values Example');
+LogMessage(`Values Jet : ${Object.values(jet)}`);
+LogMessage();
+
