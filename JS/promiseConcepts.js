c28c9358259800b06a319d64cd0632ca612fa9e9
TMizzan <terrellrhodes@hotmail.com>
2023-12-20 15:54:47 -0500

    Promise Concepts Update
    Promise Concepts Update

--- JS/promiseConcepts.js
+++ JS/promiseConcepts.js
@@ -4,9 +4,8 @@ import { CheckNodeJSVersion, DisplayNodeJSInformation, GetRandomNumber, LogMessa
 DisplayNodeJSInformation();
 
 
-// Promise
-LogMessage("Promise Example");
-LogMessage();
+// Promise (Resolve, Reject, Then, Catch, Finally)
+LogMessage("Promise (Resolve, Reject, Then, Catch, Finally) Example");
 
 let vehiclePurchasePromise = new Promise(function (resolve, reject) { 
     
@@ -27,7 +26,7 @@ vehiclePurchasePromise.
               LogMessage(`VehiclePurchasePromise - Success Message : ${successMessage}`);
           }, 
           function (rejectMessage) {
-              LogMessage(`VehiclePurchasePromise - Reject Message  : ${rejectMessage}`);
+              LogMessage(`VehiclePurchasePromise - Reject Message : ${rejectMessage}`);
           }
         )
         .catch(function(errorMessage) {
@@ -39,20 +38,65 @@ vehiclePurchasePromise.
 
 LogMessage();
 
+
+// Promise All
+const vehicleMaintenancePromise = new Promise(function (resolve, reject) { 
+    setTimeout(resolve, 200, "[VehicleMaintenancePromise - Vehicle Maintenance Complete.]");
+});
+const vehicleRepairPromise = new Promise(function (resolve, reject) { 
+    setTimeout(resolve, 200, "[VehicleRepairPromise      - Vehicle Repair Complete.]");
+});
+
+Promise.all([vehicleMaintenancePromise, vehicleRepairPromise]).
+  then((values) => {
+    LogMessage();
+    LogMessage("Promise All Example");
+    LogMessage(`All Promises Complete`)
+    values.forEach((value) => LogMessage(`All Promises Return Value : ${value}`));
+    });
+
+
+// Promise AllSettled
+Promise.allSettled([vehicleMaintenancePromise, vehicleRepairPromise]).
+  then((values) => {
+    LogMessage();
+    LogMessage("Promise All Settled Example");
+    LogMessage(`All Settled Promises Complete.`)
+    values.forEach((value) => LogMessage(`All Promises Settled Return : Status - ${value.status}, Value - ${value.value}`));
+    });
+
+
+// Promise Any
+const vehicleRejectPromise = Promise.reject("Vehicle Out Of Service");
+const vehicleFuelPromise = new Promise(function (resolve, reject) { 
+    setTimeout(reject, 800, "[VehicleFuelPromise      - Vehicle Fuel Fill Incomplete.]");
+});
+
+Promise.any([vehicleRejectPromise, vehicleFuelPromise]).
+    then((values) => {
+        LogMessage();
+        LogMessage("Promise Any Example");
+        LogMessage();
+    })
+    .catch((ex) => {
+        LogMessage();
+        LogMessage(`Promise Any Example - Error Message : ${ex}.`)
+    });
+
+
 // Promise Race
 let vehicleRacePromise1 = new Promise(function (resolve, reject) { 
-    setTimeout(resolve, 500, "VehicleRacePromise1 - First Promise Complete.")
+    setTimeout(resolve, 800, "VehicleRacePromise1 - First Promise Complete.")
 });
 
 let vehicleRacePromise2 = new Promise(function (resolve, reject) { 
-    setTimeout(resolve, 100, "VehicleRacePromise1 - Second  Promise Complete.")
+    setTimeout(resolve, 200, "VehicleRacePromise2 - Second Promise Complete.")
 });
 
-Promise.race([vehicleRacePromise1], [vehicleRacePromise2]).
+Promise.race([vehicleRacePromise1, vehicleRacePromise2]).
     then((value) => { 
         LogMessage();
         LogMessage("Promise Race Example");
-        LogMessage();
         LogMessage(`VehicleRacePromise - ${value}`);
     });
 
